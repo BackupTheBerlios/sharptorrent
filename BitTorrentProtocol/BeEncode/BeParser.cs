b974e8f61de873e8fd58a801e034272625a1cddc
youp <youp>
2005-02-11 14:10:25 +0000

    no message

--- BitTorrentProtocol/BeEncode/BeParser.cs
+++ BitTorrentProtocol/BeEncode/BeParser.cs
@@ -16,11 +16,38 @@ namespace SharpTorrent.BitTorrentProtocol.BeEncode {
         }
     }
     public class BeParser {
+        private int actualTokenPos = 0;
+        private int nextTokenPos = 1;
+        private BeEncode.Dictionary metainfo;
+        private string metainfoText;
+
         public BeParser() {
+            metainfo = new BeEncode.Dictionary();
         }
 
-        public BeEncode.Dictionary Parse() {
-            return null;
+        private BeEncode.String ParseString() {
+
+        }
+
+        private BeEncode.Integer ParseString() {
+
+        }
+
+        private BeEncode.List ParseString() {
+
+        }
+
+        private BeEncode.Dictionary ParseString() {
+
+        }
+
+        public BeEncode.Dictionary Parse(string Text) {
+            metainfoText = Text;
+            // The begining is a 'd'
+            if ((char)metainfoText[actualTokenPos] != 'd')
+                throw new BeParserException("This is not a valid dictionary.");
+            
+            return metainfo;
         }
     }
 }
