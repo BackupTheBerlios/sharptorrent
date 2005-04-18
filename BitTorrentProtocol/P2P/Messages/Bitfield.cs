b95fbeb9e5a1388132ebb68ca2094a95052309fc
youp <youp>
2005-04-18 18:50:56 +0000

    *** empty log message ***

--- BitTorrentProtocol/P2P/Messages/Bitfield.cs
+++ BitTorrentProtocol/P2P/Messages/Bitfield.cs
@@ -4,44 +4,48 @@ using SharpTorrent.BitTorrentProtocol.Utilities;
 
 namespace SharpTorrent.BitTorrentProtocol.P2P.Messages {
 	/// <summary>
-	/// 'bitfield' (5) is only ever sent as the first message. 
+	/// 'bitfield' is only ever sent as the first message. 
 	/// Its payload is a bitfield with each index that downloader has sent set to one 
 	/// and the rest set to zero. 
 	/// 
-  ///	Downloaders which don't have anything yet may skip the 'bitfield' message. 
-  ///	The first byte of the bitfield corresponds to indices 0 - 7 from high bit to low bit,
-  ///	respectively. The next one 8-15, etc. Spare bits at the end are set to zero.
-	/// </summary>
+    ///	Downloaders which don't have anything yet may skip the 'bitfield' message. 
+    ///	The first byte of the bitfield corresponds to indices 0 - 7 from high bit to low bit,
+    ///	respectively. The next one 8-15, etc. Spare bits at the end are set to zero.
+	/// Example:
+    /// 0 0 0 13 5 255 255 255 255 255 255 255 255 255 255 255 224
+    /// </summary>
     public class Bitfield : Message, IMessage {
-        private const int BITFIELDMESSAGELENGTH = BigEndian.BIGENDIANBYTELENGTH + 1;
-        private ArrayList pieces;
         private int numPieces;
+        private byte[] pieces;
 
-        /* 0 0 0 13 5 255 255 255 255 255 255 255 255 255 255 255 224 */
 
-        #region Constructors
-
-        public Bitfield() {
-            this.type = 5;
-            numPieces = 0;
-            pieces = new ArrayList();
-        }
-
-        public Bitfield(int numberOfPieces) : base() {
-            numPieces = numberOfPieces;
-        }
-
-        public Bitfield(Bitfield origin) {
+        public Bitfield(int numPieces) {
+            this.numPieces = numPieces;
+            // Create the byte array. 8 pieces = 1 Byte
+            int numBytes = (numPieces / sizeof(byte));
+            if ((numPieces % sizeof(byte)) != 0)
+                numBytes++;
+            pieces = new byte[numPieces];
 
         }
 
-        #endregion
+        public Bitfield(byte [] buffer) {
 
-        #region  Public methods
+        }
+
+        public Bitfield(Bitfield bitfield) {
+
+        }
+
+        #region Public Methods
 
         public void AddPiece(int pieceIndex) {
-            if (!pieces.Contains(pieceIndex))
-                pieces.Add(pieceIndex);
+
+        }
+
+        public bool HasPiece(int pieceIndex) {
+
+            return false;
         }
 
         #endregion
@@ -49,15 +53,7 @@ namespace SharpTorrent.BitTorrentProtocol.P2P.Messages {
         #region IMessage Members
 
         byte[] IMessage.ToStream() {
-            // Num bytes to store the bitField (numPieces / 32)
-            int numBytesOnMessage = ((int) numPieces / 32) + ((int) numPieces % 32);
-            this.message = new byte[BITFIELDMESSAGELENGTH + numBytesOnMessage];
-            byte[] messageLength = BigEndian.ToBigEndian(1 + numBytesOnMessage);
-            AddMessage(message, messageLength);
-            AddMessage(message, type);
-            // Convert each piece
-
-            return message;
+            throw new NotImplementedException();
         }
 
         #endregion
